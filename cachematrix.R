fc8cb841920f06990a71cf4b353e35fbfa11bcf5
Fernandez Cortes <richard.fernandez.cortes@acciona.com>
2016-05-04 12:02:26 +0200

    added functions code

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix() creates a special matrix 
+## that contains a list of functions to get, set data and get and set the solve
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
 ## Write a short comment describing this function
-
+## cacheSolve look if solve was previously calculated and returns it.
+## if not, calculates and return 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+          message("getting cached solve matrix")
+          return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
